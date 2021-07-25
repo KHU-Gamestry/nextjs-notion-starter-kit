7e4bb062c71f37624c5e64870de6a4a9204b49ed
hyun <seok02h@khu.ac.kr>
2021-07-25 23:17:06 +0900

    fix code

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -207,7 +207,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
             <meta property='twitter:url' content={canonicalPageUrl} />
           </>
         )}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
         <title>{title}</title>
       </Head>
 
